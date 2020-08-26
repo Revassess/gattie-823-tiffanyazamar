a88f175c24bf1e2cb1e82b0d9159162d528d7544
Tiffany <eunicefazamar@gmail.com>
2020-08-26 13:46:19 -0600

    finished with tier3

--- Revassess/src/main/java/com/revature/config/ConnectionUtil.java
+++ Revassess/src/main/java/com/revature/config/ConnectionUtil.java
@@ -16,15 +16,15 @@ public class ConnectionUtil {
 	private static ConnectionUtil cu;
 	
 	// add your jdbc url
-	public static final String URL = "";
+	public static final String URL = "jdbc:postgresql://javafs200803.ch6hwzkmseno.us-east-2.rds.amazonaws.com:5432/tier3";
 	// add your jdbc username
-	public static final String USERNAME = "";
+	public static final String USERNAME = "postgres";
 	// add your jdbc password
-	public static final String PASSWORD = "";
+	public static final String PASSWORD = "eunice0426";
 	// name of the created stored procedure in tier 3
 	public static final String TIER_3_PROCEDURE_NAME = "";
 	// name of the created sequence in tier 3
-	public static final String TIER_3_SEQUENCE_NAME = "";
+	public static final String TIER_3_SEQUENCE_NAME = "mysequence";
 
 	// implement this method to connect to the db and return the connection object
 	public Connection connect(){

dced192e8a05d600e21ce2391b545e3eaa9c4c8e
Tiffany <eunicefazamar@gmail.com>
2020-08-26 12:31:55 -0600

    All tests passed but PointTests failed :(

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,17 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
+		String stringNumber = String.valueOf(num);
+		int length = stringNumber.length();
+		char[] digits = stringNumber.toCharArray();
+		int sumOfPower = 0;
+		for(int i = 0; i<digits.length; i++) {
+			int intDigit = Character.getNumericValue(digits[i]);
+			sumOfPower += Math.pow(intDigit, length);
+		}
+		if(sumOfPower==num) {
+			return true;
+		}
 		return false;
 	}
 }
--- Revassess/src/main/java/com/revature/tier1/User.java
+++ Revassess/src/main/java/com/revature/tier1/User.java
@@ -1,4 +1,59 @@
 package com.revature.tier1;
 
 public class User {
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String password;
+	private String role;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
 }
